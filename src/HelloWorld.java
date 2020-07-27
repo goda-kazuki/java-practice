7bb83aba56c0df3aa1d55db279379d91183e338d
goda-kazuki <goda@eyemovic.com>
2020-07-27 15:59:11 +0900

    use variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("はろーわーるど！");
+        int height = 300;
+        int weight = 200;
+        System.out.println(height + weight);
+
+        String halloWorld = "はろーわーるど";
+
+        System.out.println(halloWorld);
     }
 }
