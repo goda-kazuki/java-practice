3cf8c637b1275c41f2738a340012eab7cf8427d4
goda-kazuki <goda@eyemovic.com>
2020-07-29 10:27:11 +0900

    メソッドに可変で引数を入れることができる

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,13 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        Empolyee empolyee = new Empolyee(24, "ごうだ", 100);
+        myPrint(1, "3", "ご", "なな");
+        myPrint("いっち");
+    }
 
-        System.out.println(empolyee.getEmpolyeeID());
-
-        System.out.println(empolyee.getName());
-
-        System.out.println(empolyee.getAge());
-
+    public static void myPrint(Object... args) {
+        for (Object s : args) {
+            System.out.println(s);
+        }
     }
 }
