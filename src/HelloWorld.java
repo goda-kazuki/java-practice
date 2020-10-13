504ffd27f4019609df7a3234bb732563e0b6afee
goda-kazuki <goda@eyemovic.com>
2020-10-13 13:49:28 +0900

    List型

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String[] result = {"a", "b", "c"};
+        List<String> list = new ArrayList<String>() {
+            {
+                add("D");
+                add("E");
+                add("F");
 
+            }
+        };
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(result[i]);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
+
     }
 }
