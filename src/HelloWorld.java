9c0a31eabb619b993a204b6aa81ae7e33ce9d2f5
goda-kazuki <goda@eyemovic.com>
2020-10-13 13:39:32 +0900

    javaの配列

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        String[] result = {"a", "b", "c"};
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
